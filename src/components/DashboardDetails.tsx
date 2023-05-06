a56d2ba4c9d68c3c0bd29271acc613a73ff42cee
Sahil9824 <sahilsharma5354@gmail.com>
2023-05-06 20:51:57 +0530

    "dashboard Item Done"

---
+++ src/components/DashboardDetails.tsx
@@ -0,0 +1,133 @@
+import React from "react";
+import TriangleError from "../../assets/svgs/TriangleError";
+import PieLoader from "../../assets/svgs/PieLoader";
+import Tick from "../../assets/svgs/Tick";
+import Clock from "../../assets/svgs/Clock";
+import Error from "../../assets/svgs/Error";
+import Gas from "../../assets/svgs/Gas";
+import SmallGas from "../../assets/svgs/SmallGas";
+import CryptoSymbol from "../../assets/svgs/CryptoSymbol";
+
+interface Props {
+  isTypeOne?: boolean;
+  isInterrupted?: boolean;
+  isCompleted?: boolean;
+  isRunning?: boolean;
+  isAddFunds?: boolean;
+  date?: string;
+  time?: string;
+  btn1?: string;
+  btn2?: string;
+}
+
+const DashboardDetails: React.FC<Props> = ({
+  isInterrupted,
+  isCompleted,
+  isRunning,
+  isAddFunds,
+  date,
+  time,
+  btn1,
+  btn2,
+}) => {
+  return (
+    <div className="relative w-full py-[24px] px-[24px] search_border dashDetailBg my-[8px]">
+      <div className="w-3/4">
+        <div className="flex items-center absolute left-0 top-[24px]">
+          <div className="mr-[8px] w-[105px] flex items-center justify-end bg-[#DF4141] pr-[12px] rounded-r-[8px]">
+            <div>
+              {isInterrupted && <TriangleError />}
+              {isRunning && <PieLoader />}
+              {isCompleted && <Tick />}
+            </div>
+            <p className="font-Satoshi_700 text-[12px] text-white ml-[4px]">
+              Create
+            </p>
+          </div>
+          <p className="flex items-center ">
+            <Clock />
+            <p className="font-Satoshi_400 text-[14px] text-white/70 ml-[8px]">
+              {date} {time}
+            </p>
+          </p>
+        </div>
+        <p className="font-Satoshi_400 text-[16px] text-white mt-[36px]">
+          {isAddFunds ? "Using Card" : "Beep"}
+        </p>
+        <div className="flex mt-[4px] items-center">
+          <BeepText />
+          <BeepText />
+          <BeepText />
+          <p className="font-Satoshi_700 text-[14px] text-white ml-[8px] mt-[14px]">
+            +4
+          </p>
+        </div>
+        <div className="flex items-center mt-[18px]">
+          {!isAddFunds ? (
+            <div className="relative search_border px-[16px] h-[29px] w-[289px] flex items-center ">
+              <p className="flex items-center font-Satoshi_400 text-[14px] text-white border_right_grey pr-[16px]">
+                0/7
+                <p className="ml-[8px] ">Minting</p>
+              </p>
+              <p className="pl-[16px] flex items-center font-Satoshi_400 text-[14px] text-white border_right_grey">
+                0<p className="ml-[8px] text-white/70 pr-[16px]">Success</p>
+              </p>
+              <p className="flex items-center font-Satoshi_400 text-[14px] pl-[16px] text-white ">
+                7<p className="ml-[8px] text-white/70 pr-[16px]">Failed</p>
+              </p>
+              <div className="absolute right-[-9px] top-[-9px]">
+                <Error />
+              </div>
+            </div>
+          ) : (
+            <p className="flex px-[16px] items-center search_border font-Satoshi_400 text-[14px] text-white border_right_grey py-[6px]">
+              <CryptoSymbol />
+              <p className="ml-[8px] ">1.5 ETH</p>
+            </p>
+          )}
+          {!isAddFunds && (
+            <p className="flex items-center ml-[14px]">
+              <SmallGas />
+
+              <span className="pl-[8px] font-Satoshi_400 text-[14px] text-white ">
+                0 ETH $0 USD
+              </span>
+            </p>
+          )}
+        </div>
+        {!isAddFunds && <ProgressBar />}
+      </div>
+
+      <div className="mt-[16px] flex items-center">
+        {btn1 && (
+          <div className="font-Satoshi_400 text-[14px]  cursor-pointer bg-white text-[#121212] px-[16px] py-[8px] rounded-[8px] w-fit mr-[8px]">
+            {btn1}
+          </div>
+        )}
+        {btn2 && (
+          <div className="font-Satoshi_400 text-[14px]  cursor-pointer text-white border border-solid border-white px-[16px] py-[8px] rounded-[8px] w-fit">
+            {btn2}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const BeepText = () => (
+  <p className="font-Satoshi_700 text-[30px] text-white mr-[4px]">
+    Beep 057{" "}
+    <span className="font-Satoshi_700 text-[14px] text-white">(1)</span>
+    <span className="font-Satoshi_700 text-[14px] text-white/50 ml-[8px]">
+      /
+    </span>
+  </p>
+);
+
+const ProgressBar = () => (
+  <div className="h-[16px] relative rounded-[8px] w-full bg-black/10 mt-[16px] ">
+    <div className="h-[16px] w-1/2 bg-white absolute left-0 top-0 rounded-[8px] gradInItem"></div>
+  </div>
+);
+
+export default DashboardDetails;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import TradeIn from "../../assets/svgs/TradeIn";
 import Analytics from "../../assets/svgs/Analytics";
 import { map } from "lodash";
 import { DASHBOARD_ITEM_COLORS } from "../general.constants";
+import DashboardDetails from "@/components/DashboardDetails";
 
 const DashboardItemsList = [
   {
@@ -39,7 +40,7 @@ const DashboardItemsList = [
 
 const Homepage = () => {
   return (
-    <div className="bg-c_bg_black w-screen h-screen ">
+    <div className="bg-c_bg_black w-screen h-screen overflow-scroll">
       <Navbar />
       <div className="flex  w-full justify-between items-center mt-[16px] gap-[16px] px-6">
         {map(DashboardItemsList, (item: DashboardProps) => (
@@ -49,30 +50,59 @@ const Homepage = () => {
       <div className="border_bottom w-screen mt-[16px]"></div>
 
       <div className="flex w-full mr-[24px] px-[24px] mt-[36px]">
-        <div className="flex items-center  w-9/12">
-          <p className="flex font-Satoshi_400 text-white/50 text-[24px]">
-            Recent Activity
-          </p>
-          <div className="flex items-center justify-center ml-[8px]">
-            <div className="h-[12px] w-[12px] rounded-[6px] bg-[#70BC63] mr-[8px]"></div>
-            <p className="font-Satoshi_700 text-[14px] text-white leading-[21px]">
-              Running
+        <div className="flex flex-col w-9/12 pr-[24px]">
+          <div className="flex items-center">
+            <p className="flex font-Satoshi_400 text-white/50 text-[24px]">
+              Recent Activity
             </p>
+            <div className="flex items-center justify-center ml-[8px]">
+              <div className="h-[12px] w-[12px] rounded-[6px] bg-[#70BC63] mr-[8px]"></div>
+              <p className="font-Satoshi_700 text-[14px] text-white leading-[21px]">
+                Running
+              </p>
+            </div>
+            <div className="flex items-center justify-center ml-[8px]">
+              <div className="h-[12px] w-[12px] rounded-[6px] bg-[#DF4141] mr-[8px]"></div>
+              <p className="font-Satoshi_700 text-[14px] text-white leading-[21px]">
+                Interrupted
+              </p>
+            </div>
+            <div className="flex items-center justify-center ml-[8px]">
+              <div className="h-[12px] w-[12px] rounded-[6px] bg-[#4798E3] mr-[8px]"></div>
+              <p className="font-Satoshi_700 text-[14px] text-white leading-[21px]">
+                Completed
+              </p>
+            </div>
           </div>
-          <div className="flex items-center justify-center ml-[8px]">
-            <div className="h-[12px] w-[12px] rounded-[6px] bg-[#DF4141] mr-[8px]"></div>
-            <p className="font-Satoshi_700 text-[14px] text-white leading-[21px]">
-              Interrupted
-            </p>
-          </div>
-          <div className="flex items-center justify-center ml-[8px]">
-            <div className="h-[12px] w-[12px] rounded-[6px] bg-[#4798E3] mr-[8px]"></div>
-            <p className="font-Satoshi_700 text-[14px] text-white leading-[21px]">
-              Completed
-            </p>
-          </div>
+          <DashboardDetails
+            isInterrupted
+            date="2022-09-13"
+            time="08:57:15 PST"
+            btn2="Show Report"
+          />
+          <DashboardDetails
+            isRunning
+            date="2022-09-13"
+            time="08:57:15 PST"
+            btn1="View Progress"
+            btn2="Stop Mining"
+          />
+          <DashboardDetails
+            isCompleted
+            date="2022-09-13"
+            time="08:57:15 PST"
+            btn1="View in NFT Management"
+            btn2="Show Report"
+          />
+          <DashboardDetails
+            isCompleted
+            isAddFunds
+            date="2022-09-13"
+            time="08:57:15 PST"
+            btn1="View Transaction"
+          />
         </div>
-        <div className="">
+        <div className="font-Satoshi_400 text-[24px] text-white/50">
           <p>Overview</p>
         </div>
       </div>
